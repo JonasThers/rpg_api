f6c2536f559d0cbedf261fc08b73c05b0112b411
Jonas <40244574+JonasThers@users.noreply.github.com>
2024-05-12 21:32:33 +0300

    Set up remaining CRUD for characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,13 +1,35 @@
 class CharactersController < ApplicationController
   def index
-    @characters = Character.all
+    characters = Character.all
 
-    render json: @characters
+    render json: characters
   end
 
   def show
-    @character = Character.find(params[:id])
+    character = Character.find(params[:id])
 
-    render json: @character
+    render json: character
+  end
+
+  def create
+    character = Character.create!(character_params)
+    render json: character, status: :created
+  end
+
+  def update
+    character = Character.find(params[:id])
+    character.update!(character_params)
+    render json: character
+  end
+
+  def destroy
+    Character.find(params[:id]).destroy
+    head :no_content
+  end
+  
+  private
+  
+  def character_params
+    params.permit(:name, :background_story)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root "characters#index"
 
-  get "/characters", to: "characters#index"
-  get "/characters/:id", to: "characters#show"
+  resources :characters
 end
