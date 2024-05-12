322ba483bc6d0dc4e90f344799b61ad4f5eeae39
Jonas <40244574+JonasThers@users.noreply.github.com>
2024-05-12 21:35:09 +0300

    Set up validations for character class

--- app/models/character_class.rb
+++ app/models/character_class.rb
@@ -1,2 +1,8 @@
 class CharacterClass < ApplicationRecord
+    validates :name, presence: true
+    validates :strength, presence: true
+    validates :dexterity, presence: true
+    validates :wisdom, presence: true
+    validates :intelligence, presence: true
+    validates :charisma, presence: true
 end
