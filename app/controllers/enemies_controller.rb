92fb773e3af9896a0ff6465f878c42f08aceb0ce
Jonas <40244574+JonasThers@users.noreply.github.com>
2024-05-16 22:17:14 +0300

    Add CRUD for enemies

--- app/controllers/enemies_controller.rb
+++ app/controllers/enemies_controller.rb
@@ -1,4 +1,35 @@
 class EnemiesController < ApplicationController
   def index
+    enemies = Enemy.all
+
+    render json: enemies
+  end
+
+  def show
+    character = Enemy.find(params[:id])
+
+    render json: character
+  end
+
+  def create
+    character = Enemy.create!(character_params)
+    render json: character, status: :created
+  end
+
+  def update
+    character = Enemy.find(params[:id])
+    character.update!(character_params)
+    render json: character
+  end
+
+  def destroy
+    Enemy.find(params[:id]).destroy
+    head :no_content
+  end
+  
+  private
+  
+  def character_params
+    params.permit(:name, :strength, :dexterity, :wisdom, :intelligence, :charisma, :hit_points)
   end
 end
